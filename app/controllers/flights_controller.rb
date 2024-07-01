cbb83c847b0fd589d300d0c0df7ab8751441a0a1
Eddie Wen <405139216@qq.com>
2024-07-01 15:47:32 +0800

    finished search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,10 +1,13 @@
 class FlightsController < ApplicationController
   def index
-    @airport_options = Airport.all.map { |airport| airport.code }
-    @flights = Flight.where(
-      departure_airport_id: params[:departure_airport_id],
-      arrival_airport_id: params[:arrival_airport_id],
-      date: params[:flight_date]
-    )
+    @airport_options = Airport.all.map { |airport| [airport.name, airport.id] }
+    @passenger_options = [1, 2, 3, 4]
+    @date_options = Flight.order(:date).pluck(:date).map { |d| d.to_date }.uniq.map { |d| [d.strftime('%Y-%m-%d'), d] }
+
+    @flights = if params[:search]
+                 Flight.search(params).includes(:departure_airport, :arrival_airport)
+               else
+                 Flight.includes(:departure_airport, :arrival_airport).all
+               end
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,4 +3,16 @@ class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: 'Airport'
 
   validates :date, :duration_in_minutes, presence: true
+
+  scope :on_date, ->(date) { where(date: date.beginning_of_day..date.end_of_day) }
+  scope :from_airport, ->(airport_id) { where(departure_airport_id: airport_id) }
+  scope :to_airport, ->(airport_id) { where(arrival_airport_id: airport_id) }
+
+  def self.search(params)
+    flights = all
+    flights = flights.from_airport(params[:departure_airport_id]) if params[:departure_airport_id].present?
+    flights = flights.to_airport(params[:arrival_airport_id]) if params[:arrival_airport_id].present?
+    flights = flights.on_date(params[:flight_date].to_date) if params[:flight_date].present?
+    flights
+  end
 end
